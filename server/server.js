0cddcd48566c9a307bac3c01017e2f9aa885fe18
Ryan Achten <ryanachten@gmail.com>
2017-09-01 16:01:39 +1200

    Update port var for heroku and loccal server

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 
 app.use(express.static(publicPath));
 
 
-app.listen(3000, ()=>{
-  console.log('App is up on port 3000');
+app.listen(port, ()=>{
+  console.log(`App is up on port ${port}`);
 });
