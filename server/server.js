414b35a76e341d6d2c11042ee795d4a93b418f86
Ryan Achten <ryanachten@gmail.com>
2017-09-03 12:18:49 +1200

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -26,6 +26,10 @@ io.on('connection', (socket)=>{
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This from the server');
+  });
+
+  socket.on('createLocationMessage', (coords, callback)=>{
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', ()=>{
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', ()=>{
     it('should generate the correct message object', () => {
@@ -8,12 +8,27 @@ describe('generateMessage', ()=>{
       var text = 'This is some test text';
       var message = generateMessage(name, text);
 
-      // expect(message.from).toBe(name);
-      // expect(message.text).toBe(text);
       expect(message).toInclude({
         from: name,
         text
       });
       expect(message.createdAt).toBeA('number');
     });
+});
+
+describe('generateLocationMessage', ()=>{
+  it('should generate the correct location object', ()=>{
+
+    var name = 'Jane';
+    var lat = -10;
+    var long = 20;
+    var url = `https://www.google.com/maps?q=${lat},${long}`;
+
+    var message = generateLocationMessage(name, lat, long);
+
+    expect(message).toInclude({
+      from: name,
+      url: url
+    });
+  });
 });
